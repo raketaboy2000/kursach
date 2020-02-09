23d46d68cfaf70a0ddb02296a5b0f25108eb1cc5
Ruslan Shayahmetov <raketaboy2000@yandex.ru>
2020-02-09 19:15:37 +0300

    доставка

---
+++ views/site/delivery.php
@@ -0,0 +1,29 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+    <!--<section>-->
+    <div class="container">
+
+
+        <div>
+            <h3>Доставка и оплата</h3>
+
+
+            <hr>
+            <h4>Часы работы доставки</h4>
+            <p>Служба доставки работает круглосуточно</p>
+
+
+            <script type="text/javascript" charset="utf-8" async
+                    src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A1163193927e2f0b8c999440402a6b001c12c04819e1c84fae8d9147555649877&amp;width=100%25&amp;height=720&amp;lang=ru_RU&amp;scroll=true"></script>
+
+
+        </div>
+
+
+
+
+
+    </div>
+    <!--</section>-->
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
